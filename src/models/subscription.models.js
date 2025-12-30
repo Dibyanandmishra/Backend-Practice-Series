f4ce22eb165a21af444f574e2b7ed491c88741ac
Simple-Calculator <dibyanandmishra@gmail.com>
2025-12-30 22:03:23 +0530

    subscription.controller.js logic

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -3,12 +3,20 @@ import mongoose, {Schema} from "mongoose";
 const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing 
-        ref: "User"
+        ref: "User",
+        required: "true"
     },
     channel: {
         type: Schema.Types.ObjectId, // one to whom "subscriber" is subscribing 
-        ref: "User"
+        ref: "User",
+        required: "true"
     }
 }, {timestamps: true})
+
+// unique index in MongoDB to prevent duplicate subscriptions:
+subscriptionSchema.index(
+  { channel: 1, subscriber: 1 },
+  { unique: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
